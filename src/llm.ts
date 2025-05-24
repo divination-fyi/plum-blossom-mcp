e3632c717776a16624628820e0db0b997f4d9d0e
lirc572 <admin@lirc572.com>
2025-05-25 05:07:45 +0800

    feat: temporarily comment out use of cloudflare ai gateway

--- src/llm.ts
+++ src/llm.ts
@@ -1,32 +1,32 @@
-import { createAiGateway } from "ai-gateway-provider";
+// import { createAiGateway } from "ai-gateway-provider";
 import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 
-// Ensure that the necessary environment variables are set
-const CLOUDFLARE_ACCOUNT_ID = Deno.env.get("CLOUDFLARE_ACCOUNT_ID");
-const CLOUDFLARE_AI_GATEWAY_ID = Deno.env.get("CLOUDFLARE_AI_GATEWAY_ID");
-const CLOUDFLARE_AI_GATEWAY_API_KEY = Deno.env.get(
-  "CLOUDFLARE_AI_GATEWAY_API_KEY",
-);
+// const CLOUDFLARE_ACCOUNT_ID = Deno.env.get("CLOUDFLARE_ACCOUNT_ID");
+// const CLOUDFLARE_AI_GATEWAY_ID = Deno.env.get("CLOUDFLARE_AI_GATEWAY_ID");
+// const CLOUDFLARE_AI_GATEWAY_API_KEY = Deno.env.get(
+//   "CLOUDFLARE_AI_GATEWAY_API_KEY",
+// );
+// if (
+//   !CLOUDFLARE_ACCOUNT_ID || !CLOUDFLARE_AI_GATEWAY_ID ||
+//   !CLOUDFLARE_AI_GATEWAY_API_KEY
+// ) {
+//   throw new Error(
+//     "Missing required environment variables for Cloudflare AI Gateway",
+//   );
+// }
+
 const OPENROUTER_API_KEY = Deno.env.get("OPENROUTER_API_KEY");
-if (
-  !CLOUDFLARE_ACCOUNT_ID || !CLOUDFLARE_AI_GATEWAY_ID ||
-  !CLOUDFLARE_AI_GATEWAY_API_KEY
-) {
-  throw new Error(
-    "Missing required environment variables for Cloudflare AI Gateway",
-  );
-}
 if (!OPENROUTER_API_KEY) {
   throw new Error(
     "Missing required environment variables for OpenRouter",
   );
 }
 
-const aigateway = createAiGateway({
-  accountId: CLOUDFLARE_ACCOUNT_ID,
-  gateway: CLOUDFLARE_AI_GATEWAY_ID,
-  apiKey: CLOUDFLARE_AI_GATEWAY_API_KEY,
-});
+// const aigateway = createAiGateway({
+//   accountId: CLOUDFLARE_ACCOUNT_ID,
+//   gateway: CLOUDFLARE_AI_GATEWAY_ID,
+//   apiKey: CLOUDFLARE_AI_GATEWAY_API_KEY,
+// });
 
 const openrouter = createOpenRouter({
   apiKey: OPENROUTER_API_KEY,
